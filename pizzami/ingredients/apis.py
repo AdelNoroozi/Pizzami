b65de2ead01a27a8ec77104652b70d97d9e8e8ef
adel13no@gmail.com <adel13no@gmail.com>
2023-10-26 20:26:43 +0330

    feat(back): create ingredient category API & documentations added

--- pizzami/ingredients/apis.py
+++ pizzami/ingredients/apis.py
@@ -3,16 +3,31 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from pizzami.api.mixins import ApiOptionalAuthMixin
-from pizzami.ingredients.documentation import GET_INGREDIENT_CATEGORIES_RESPONSE
-from pizzami.ingredients.services import get_ingredient_categories
+from pizzami.api.mixins import ApiAuthMixin, BasePermissionsMixin
+from pizzami.ingredients.documentation import GET_INGREDIENT_CATEGORIES_200_RESPONSE
+from pizzami.ingredients.documentation.ingredient_category import CREATE_INGREDIENT_CATEGORY_400_RESPONSE, \
+    CREATE_INGREDIENT_CATEGORY_201_RESPONSE, CREATE_INGREDIENT_CATEGORY_401_RESPONSE, \
+    CREATE_INGREDIENT_CATEGORY_403_RESPONSE
+from pizzami.ingredients.serializers import IngredientCategoryInputSerializer
+from pizzami.ingredients.services import get_ingredient_categories, create_ingredient_category
 
 
-class IngredientCategoryAPI(ApiOptionalAuthMixin, APIView):
+class IngredientCategoryAPI(ApiAuthMixin, BasePermissionsMixin, APIView):
 
     @extend_schema(
-        responses={200: GET_INGREDIENT_CATEGORIES_RESPONSE})
+        responses={200: GET_INGREDIENT_CATEGORIES_200_RESPONSE}
+    )
     def get(self, request):
-        print(request.user.is_staff)
         data = get_ingredient_categories(request.user.is_staff)
         return Response(data=data, status=status.HTTP_200_OK)
+
+    @extend_schema(
+        request=IngredientCategoryInputSerializer,
+        responses={201: CREATE_INGREDIENT_CATEGORY_201_RESPONSE,
+                   400: CREATE_INGREDIENT_CATEGORY_400_RESPONSE,
+                   401: CREATE_INGREDIENT_CATEGORY_401_RESPONSE,
+                   403: CREATE_INGREDIENT_CATEGORY_403_RESPONSE}
+    )
+    def post(self, request):
+        ingredient_category_data = create_ingredient_category(request.data)
+        return Response(data=ingredient_category_data, status=status.HTTP_201_CREATED)
--- pizzami/ingredients/documentation/ingredient_category.py
+++ pizzami/ingredients/documentation/ingredient_category.py
@@ -1,8 +1,30 @@
+import rest_framework.serializers
 from drf_spectacular.utils import OpenApiResponse
 
 from pizzami.ingredients.serializers.ingredient_category import IngredientCategoryCompleteOutputSerializer
 
-GET_INGREDIENT_CATEGORIES_RESPONSE = OpenApiResponse(
+GET_INGREDIENT_CATEGORIES_200_RESPONSE = OpenApiResponse(
     response=IngredientCategoryCompleteOutputSerializer,
     description="created_at, updated_at & is_active fields are only visible to users with staff privileges."
 )
+
+CREATE_INGREDIENT_CATEGORY_201_RESPONSE = OpenApiResponse(
+    response=IngredientCategoryCompleteOutputSerializer,
+    description="a new ingredient category created successfully."
+)
+
+CREATE_INGREDIENT_CATEGORY_400_RESPONSE = OpenApiResponse(
+    description="input values are invalid or don't match the expected format. e.g: image alt text does not contain /"
+                "ingredient category's name."
+)
+
+
+CREATE_INGREDIENT_CATEGORY_401_RESPONSE = OpenApiResponse(
+    description="user is not authenticated"
+)
+
+
+CREATE_INGREDIENT_CATEGORY_403_RESPONSE = OpenApiResponse(
+    description="non-staff user is trying to create a new ingredient category"
+)
+
