d2732ff6235acf12c767b5f45079ac32fd023ba2
adel13no@gmail.com <adel13no@gmail.com>
2023-10-27 09:40:17 +0330

    refactor(back): base user manager moved to separate module

--- pizzami/users/models.py
+++ pizzami/users/models.py
@@ -2,40 +2,9 @@ from django.db import models
 from pizzami.common.models import BaseModel
 
 from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import BaseUserManager as BUM
 from django.contrib.auth.models import PermissionsMixin
 
-
-
-class BaseUserManager(BUM):
-    def create_user(self, email, is_active=True, is_admin=False, password=None):
-        if not email:
-            raise ValueError("Users must have an email address")
-
-        user = self.model(email=self.normalize_email(email.lower()), is_active=is_active, is_admin=is_admin)
-
-        if password is not None:
-            user.set_password(password)
-        else:
-            user.set_unusable_password()
-
-        user.full_clean()
-        user.save(using=self._db)
-
-        return user
-
-    def create_superuser(self, email, password=None):
-        user = self.create_user(
-            email=email,
-            is_active=True,
-            is_admin=True,
-            password=password,
-        )
-
-        user.is_superuser = True
-        user.save(using=self._db)
-
-        return user
+from pizzami.users.managers import BaseUserManager
 
 
 class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
@@ -63,7 +32,7 @@ class Profile(models.Model):
     bio = models.CharField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.user} >> {self.bio}"
+        return f"{self.user} profile"
 
 
 
