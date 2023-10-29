1502362a9db4f66737c705b2ee0f2195b03942d3
adel13no@gmail.com <adel13no@gmail.com>
2023-10-29 21:40:51 +0330

    feat(back): ingredient base and complete output serializers added

--- pizzami/ingredients/serializers/__init__.py
+++ pizzami/ingredients/serializers/__init__.py
@@ -1,3 +1,5 @@
 from pizzami.ingredients.serializers.ingredient_category import IngredientCategoryBaseOutputSerializer
 from pizzami.ingredients.serializers.ingredient_category import IngredientCategoryCompleteOutputSerializer
 from pizzami.ingredients.serializers.ingredient_category import IngredientCategoryInputSerializer
+from pizzami.ingredients.serializers.ingredient import IngredientBaseOutputSerializer
+from pizzami.ingredients.serializers.ingredient import IngredientCompleteOutputSerializer
---
+++ pizzami/ingredients/serializers/ingredient.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from pizzami.ingredients.models import Ingredient
+from pizzami.ingredients.serializers import IngredientCategoryBaseOutputSerializer
+
+
+class IngredientBaseOutputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        exclude = ("created_at", "updated_at", "is_active")
+
+
+class IngredientCompleteOutputSerializer(serializers.ModelSerializer):
+    category = IngredientCategoryBaseOutputSerializer(many=False)
+
+    class Meta:
+        model = Ingredient
+        fields = "__all__"
