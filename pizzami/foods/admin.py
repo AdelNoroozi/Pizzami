3afa92cbc4d3168f4f852aa6532e51ab3495331f
adel13no@gmail.com <adel13no@gmail.com>
2023-11-08 09:11:20 +0330

    feat(back): food category with inline compounds admin added

--- pizzami/foods/admin.py
+++ pizzami/foods/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from pizzami.foods.models import FoodCategory
+from pizzami.foods.models import FoodCategory, FoodCategoryCompound
 
-admin.site.register(FoodCategory)
+
+class FoodCategoryCompoundInlineAdmin(admin.TabularInline):
+    model = FoodCategoryCompound
+
+
+@admin.register(FoodCategory)
+class FoodCategoryAdmin(admin.ModelAdmin):
+    inlines = (FoodCategoryCompoundInlineAdmin,)
