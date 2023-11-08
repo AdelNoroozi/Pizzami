5b41a98ba3834a71ac0a6d11537f2c6e46e7e842
adel13no@gmail.com <adel13no@gmail.com>
2023-11-08 09:01:17 +0330

    feat(back): create food category with inline compounds API added

--- pizzami/foods/apis.py
+++ pizzami/foods/apis.py
@@ -1,10 +1,9 @@
-from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from pizzami.api.mixins import ApiAuthMixin, BasePermissionsMixin
-from pizzami.foods.services import get_food_categories
+from pizzami.foods.services import get_food_categories, create_food_category
 
 
 class FoodCategoriesAPI(ApiAuthMixin, BasePermissionsMixin, APIView):
@@ -12,3 +11,7 @@ class FoodCategoriesAPI(ApiAuthMixin, BasePermissionsMixin, APIView):
     def get(self, request):
         data = get_food_categories(is_user_staff=request.user.is_staff)
         return Response(data=data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        food_category_data = create_food_category(data=request.data)
+        return Response(data=food_category_data, status=status.HTTP_201_CREATED)
