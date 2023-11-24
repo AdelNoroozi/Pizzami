d1f042ce5eeef24cc95949013d0c32ceb39f791f
adel13no@gmail.com <adel13no@gmail.com>
2023-11-24 10:14:47 +0330

    fix(back): two minor issues in base model admin resolved

--- pizzami/common/admin.py
+++ pizzami/common/admin.py
@@ -2,7 +2,7 @@ from django.contrib.admin import ModelAdmin
 
 
 class BaseModelAdmin(ModelAdmin):
-    list_display = ["name", "is_active", "position", "created_at", "updated_at"]
+    list_display = ["position", "is_active", "created_at", "updated_at"]
     list_editable = ["is_active"]
     list_filter = ["is_active", "created_at", "updated_at"]
     date_hierarchy = "created_at"
