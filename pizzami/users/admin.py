cea01b3b16a90718f79b251657ea29a3632553fe
adel13no@gmail.com <adel13no@gmail.com>
2023-12-22 17:38:58 +0330

    feat(back): profile model registered in admin

--- pizzami/users/admin.py
+++ pizzami/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from pizzami.users.models import BaseUser
+from pizzami.users.models import BaseUser, Profile
 
 admin.site.register(BaseUser)
+admin.site.register(Profile)
