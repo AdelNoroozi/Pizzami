a0351ab43bb97cd9b348c116c6cc788abd3d20ee
adel13no@gmail.com <adel13no@gmail.com>
2024-01-24 12:33:26 +0330

    feat(back): create food service added

--- pizzami/foods/services/__init__.py
+++ pizzami/foods/services/__init__.py
@@ -4,4 +4,5 @@ from pizzami.foods.services.food_category import retrieve_food_category
 from pizzami.foods.services.food_category import update_food_category
 from pizzami.foods.services.food import get_foods
 from pizzami.foods.services.food_ingredient import create_food_ingredient
+from pizzami.foods.services.food import create_food
 
--- pizzami/foods/services/food.py
+++ pizzami/foods/services/food.py
@@ -1,14 +1,18 @@
+from django.db import transaction
 from django.http import QueryDict
-from rest_framework.utils.serializer_helpers import ReturnList
+from rest_framework.utils.serializer_helpers import ReturnList, ReturnDict
 
 from pizzami.foods.filters import FoodFilter
 from pizzami.foods.models import Food
 from pizzami.foods.selectors import get_foods as get_foods_selector, search_food, order_foods
-from pizzami.foods.serializers import FoodBaseOutputSerializer, FoodDetailedOutputSerializer
+from pizzami.foods.serializers import FoodBaseOutputSerializer, FoodDetailedOutputSerializer, FoodInputSerializer, \
+    FoodCompleteOutputSerializer
+from pizzami.foods.services import create_food_ingredient
 from pizzami.users.models import BaseUser
 
 
-def get_foods(get_method: QueryDict, is_user_staff: bool, user_created: bool, user: BaseUser = None) -> ReturnList[Food]:
+def get_foods(get_method: QueryDict, is_user_staff: bool, user_created: bool, user: BaseUser = None) -> ReturnList[
+    Food]:
     if user_created:
         queryset = get_foods_selector(return_all=False, user_profile=user.profile)
     else:
@@ -26,3 +30,16 @@ def get_foods(get_method: QueryDict, is_user_staff: bool, user_created: bool, us
     else:
         serializer = FoodBaseOutputSerializer(queryset, many=True)
     return serializer.data
+
+
+@transaction.atomic
+def create_food(data: dict, user: BaseUser) -> ReturnDict:
+    serializer = FoodInputSerializer(data=data, context={"user": user})
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    if "ingredients" in data:
+        ingredients = data.pop("ingredients")
+        for ingredient_data in ingredients:
+            create_food_ingredient(food=serializer.instance, data=ingredient_data)
+    response_serializer = FoodCompleteOutputSerializer(serializer.instance, many=False)
+    return response_serializer.data
