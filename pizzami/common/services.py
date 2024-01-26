b5e68ba497ac098ef6e5667e3a639843733bfa99
adel13no@gmail.com <adel13no@gmail.com>
2024-01-26 15:14:00 +0330

    refactor(back): common service for changing activation status of base models added

---
+++ pizzami/common/services.py
@@ -0,0 +1,13 @@
+import uuid
+
+from django.db.models.base import ModelBase
+from rest_framework.generics import get_object_or_404
+
+
+def change_activation_status(obj_id: uuid, obj_cls: ModelBase) -> bool:
+    print(type(obj_cls))
+    obj = get_object_or_404(obj_cls, id=obj_id)
+    is_active = obj.is_active
+    obj.is_active = not is_active
+    obj.save()
+    return not is_active
