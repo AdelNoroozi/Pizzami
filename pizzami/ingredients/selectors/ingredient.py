3cb95a5a42d816ec091ae0fb8e67b1e3a7a77217
adel13no@gmail.com <adel13no@gmail.com>
2024-01-31 11:05:07 +0330

    refactor(back): ingredient category activation status affects on ingredient list now

--- pizzami/ingredients/selectors/ingredient.py
+++ pizzami/ingredients/selectors/ingredient.py
@@ -7,7 +7,7 @@ def get_ingredients(return_all: bool) -> QuerySet[Ingredient]:
     if return_all:
         return Ingredient.objects.all()
     else:
-        return Ingredient.objects.active()
+        return Ingredient.objects.active().filter(category__is_active=True)
 
 
 def delete_ingredient(ingredient: Ingredient):
