4b493f1b25cd8c9b50ba429ef41dbc91ff14392d
adel13no@gmail.com <adel13no@gmail.com>
2024-02-18 21:37:33 +0330

    refactor(back): modification on generic fields representation on discount serializer

--- pizzami/orders/serializers/discount.py
+++ pizzami/orders/serializers/discount.py
@@ -1,9 +1,13 @@
+from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
 
 from pizzami.foods.models import Food, FoodCategory
+from pizzami.foods.serializers import FoodBaseOutputSerializer, FoodCategoryBaseOutputSerializer
 from pizzami.orders.models import Discount
 from pizzami.users.models import Profile
+from pizzami.users.serializers import ProfileReferenceSerializer
 
 
 class DiscountBaseOutputSerializer(serializers.ModelSerializer):
@@ -20,32 +24,26 @@ class DiscountBaseOutputSerializer(serializers.ModelSerializer):
             return f"{obj.absolute_value}$"
 
 
+class SpecifiedToRelatedField(serializers.RelatedField):
+
+    def to_representation(self, value):
+        if isinstance(value, Food):
+            serializer = FoodBaseOutputSerializer(value)
+        elif isinstance(value, Profile):
+            serializer = ProfileReferenceSerializer(value)
+        elif isinstance(value, FoodCategory):
+            serializer = FoodCategoryBaseOutputSerializer(value)
+        else:
+            return None
+        return serializer.data
+
+
 class DiscountCompleteOutputSerializer(serializers.ModelSerializer):
+    specified_to = SpecifiedToRelatedField(source="specified_object", read_only=True)
+
     class Meta:
         model = Discount
-        fields = "__all__"
+        exclude = ("object_id", "specified_type")
 
 
-class DiscountInputSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Discount
-        exclude = ("id", "is_active", "position", "created_at", "modified_at", "specified_type")
 
-    def validate(self, data):
-        if data["has_time_limit"] and (data["start_date"] is None or data["expiration_date"] is None):
-            raise ValidationError(_("time limited discounts must have start and expiration date"))
-
-        if data["type"] == Discount.TYPE_ABSOLUTE and data["absolute_value"] is None:
-            raise ValidationError(_("absolute discounts must have a absolute value"))
-
-        if data["type"] == Discount.TYPE_RATIO and data["percentage_value"] is None:
-            raise ValidationError(_("ratio discounts must have a percentage value"))
-
-        if (data["specified_to_type"] == Discount.SPECIFIED_TO_USER and
-            not isinstance(data["specified_object"], Profile)) or (
-                data["specified_to_type"] == Discount.SPECIFIED_TO_FOOD and
-                not isinstance(data["specified_object"], Food)) or (
-                data["specified_to_type"] == Discount.SPECIFIED_TO_CATEGORY and
-                not isinstance(data["specified_object"], FoodCategory)
-        ):
-            raise ValidationError(_(f"the specified id must be a type of {data['specified_to_type']}"))
