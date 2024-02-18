882cd8d5f616c8b5945d92aab17a72601f9ea14f
adel13no@gmail.com <adel13no@gmail.com>
2024-02-18 21:57:10 +0330

    feat(back): create discount service added

--- pizzami/orders/services/__init__.py
+++ pizzami/orders/services/__init__.py
@@ -1,1 +1,2 @@
 from pizzami.orders.services.discount import get_discount_list
+from pizzami.orders.services.discount import create_discount
--- pizzami/orders/services/discount.py
+++ pizzami/orders/services/discount.py
@@ -1,9 +1,10 @@
 from rest_framework.utils.serializer_helpers import ReturnList
 
 from pizzami.orders.models import Discount
-from pizzami.orders.serializers import DiscountBaseOutputSerializer, DiscountCompleteOutputSerializer
-from pizzami.users.models import BaseUser
 from pizzami.orders.selectors import get_discount_list as get_discount_list_selector
+from pizzami.orders.serializers import DiscountBaseOutputSerializer, DiscountCompleteOutputSerializer, \
+    DiscountInputSerializer
+from pizzami.users.models import BaseUser
 
 
 def get_discount_list(is_user_staff: bool, user: BaseUser = None) -> ReturnList[Discount]:
@@ -14,3 +15,11 @@ def get_discount_list(is_user_staff: bool, user: BaseUser = None) -> ReturnList[
         queryset = get_discount_list_selector(private_only=True, user=user.profile)
         serializer = DiscountBaseOutputSerializer(queryset, many=True)
     return serializer.data
+
+
+def create_discount(data: dict):
+    serializer = DiscountInputSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    response_serializer = DiscountCompleteOutputSerializer(serializer.instance, many=False)
+    return response_serializer.data
