8c01c8c95c789e1965bf42f7b1e4ed5051691679
adel13no@gmail.com <adel13no@gmail.com>
2024-02-18 20:40:22 +0330

    feat(back): discount input serializer

--- pizzami/orders/serializers/__init__.py
+++ pizzami/orders/serializers/__init__.py
@@ -1,2 +1,3 @@
 from pizzami.orders.serializers.discount import DiscountBaseOutputSerializer
 from pizzami.orders.serializers.discount import DiscountCompleteOutputSerializer
+from pizzami.orders.serializers.discount import DiscountInputSerializer
--- pizzami/orders/serializers/discount.py
+++ pizzami/orders/serializers/discount.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
+from pizzami.foods.models import Food, FoodCategory
 from pizzami.orders.models import Discount
+from pizzami.users.models import Profile
 
 
 class DiscountBaseOutputSerializer(serializers.ModelSerializer):
@@ -21,3 +24,28 @@ class DiscountCompleteOutputSerializer(serializers.ModelSerializer):
     class Meta:
         model = Discount
         fields = "__all__"
+
+
+class DiscountInputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discount
+        exclude = ("id", "is_active", "position", "created_at", "modified_at", "specified_type")
+
+    def validate(self, data):
+        if data["has_time_limit"] and (data["start_date"] is None or data["expiration_date"] is None):
+            raise ValidationError(_("time limited discounts must have start and expiration date"))
+
+        if data["type"] == Discount.TYPE_ABSOLUTE and data["absolute_value"] is None:
+            raise ValidationError(_("absolute discounts must have a absolute value"))
+
+        if data["type"] == Discount.TYPE_RATIO and data["percentage_value"] is None:
+            raise ValidationError(_("ratio discounts must have a percentage value"))
+
+        if (data["specified_to_type"] == Discount.SPECIFIED_TO_USER and
+            not isinstance(data["specified_object"], Profile)) or (
+                data["specified_to_type"] == Discount.SPECIFIED_TO_FOOD and
+                not isinstance(data["specified_object"], Food)) or (
+                data["specified_to_type"] == Discount.SPECIFIED_TO_CATEGORY and
+                not isinstance(data["specified_object"], FoodCategory)
+        ):
+            raise ValidationError(_(f"the specified id must be a type of {data['specified_to_type']}"))
