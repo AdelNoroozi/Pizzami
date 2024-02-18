1bcee909440dd955dfcc2e132b3d3938f1693c3b
adel13no@gmail.com <adel13no@gmail.com>
2024-02-18 18:45:30 +0330

    fix(back): discount selector generic FK issue resolved

--- pizzami/orders/selectors/discount.py
+++ pizzami/orders/selectors/discount.py
@@ -1,3 +1,4 @@
+from django.contrib.contenttypes.models import ContentType
 from django.db.models import QuerySet
 
 from pizzami.orders.models import Discount
@@ -6,6 +7,7 @@ from pizzami.users.models import Profile
 
 def get_discount_list(private_only: bool, user: Profile = None) -> QuerySet[Discount]:
     if private_only:
-        return Discount.objects.active().filter(is_public=False, specified_object=user)
+        contenttype_obj = ContentType.objects.get_for_model(user)
+        return Discount.objects.active().filter(is_public=False, object_id=user.id, specified_type=contenttype_obj)
     else:
         return Discount.objects.all()
