d4978ef437be9a3e8dc07647d88a25ff1889597a
adel13no@gmail.com <adel13no@gmail.com>
2024-02-20 11:49:01 +0330

    refactor(back): small modification on add to cart service for propper response

--- pizzami/orders/services/cart.py
+++ pizzami/orders/services/cart.py
@@ -2,12 +2,13 @@ from django.db import transaction
 from rest_framework.generics import get_object_or_404
 
 from pizzami.foods.models import Food
+from pizzami.orders.models import Cart
 from pizzami.orders.selectors import get_or_create_cart, get_or_create_cart_item, delete_cart_item
 from pizzami.users.models import BaseUser
 
 
 @transaction.atomic
-def add_to_cart(food_id: str, count: int, user: BaseUser):
+def add_to_cart(food_id: str, count: int, user: BaseUser) -> Cart:
     cart = get_or_create_cart(user=user.profile)
     food = get_object_or_404(Food, id=food_id)
     cart_item = get_or_create_cart_item(cart=cart, food=food)
@@ -17,3 +18,4 @@ def add_to_cart(food_id: str, count: int, user: BaseUser):
     else:
         cart_item.count = count_sum
         cart_item.save()
+    return cart
