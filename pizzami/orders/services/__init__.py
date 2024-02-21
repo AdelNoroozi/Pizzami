4cfa8db84ac48633d2593565966577b83cedfa5c
adel13no@gmail.com <adel13no@gmail.com>
2024-02-21 16:32:49 +0330

    refactor(back): submit my order service added

--- pizzami/orders/services/__init__.py
+++ pizzami/orders/services/__init__.py
@@ -3,3 +3,5 @@ from pizzami.orders.services.discount import create_discount
 from pizzami.orders.services.discount import delete_discount
 from pizzami.orders.services.discount import update_discount
 from pizzami.orders.services.cart import add_to_cart
+from pizzami.orders.services.order import create_or_update_order
+from pizzami.orders.services.order import submit_my_order
--- pizzami/orders/services/order.py
+++ pizzami/orders/services/order.py
@@ -1,3 +1,6 @@
+from django.db import transaction
+from django.utils.translation import gettext_lazy as _
+from rest_framework.generics import get_object_or_404
 from rest_framework.utils.serializer_helpers import ReturnDict
 
 from pizzami.orders.models import Order
@@ -19,3 +22,13 @@ def create_or_update_order(data: dict, user: BaseUser) -> ReturnDict[Order] | No
     serializer.save()
     response_serializer = OrderDetailedOutputSerializer(serializer.instance)
     return response_serializer.data
+
+
+@transaction.atomic
+def submit_my_order(user: BaseUser) -> (bool, str):
+    order = get_object_or_404(Order, user=user.profile, status=Order.STATUS_CREATED)
+    if order.has_delivery is None:
+        return False, _("must determine whether the order has delivery or not.")
+    order.status = Order.STATUS_READY_TO_PAY
+    order.save()
+    return True, _("done")
