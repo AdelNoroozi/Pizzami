b24b615093849f09a33c3662e4c738db2468d26a
adel13no@gmail.com <adel13no@gmail.com>
2024-02-21 12:23:37 +0330

    fix(back): cart process added to create or update order service

--- pizzami/orders/services/order.py
+++ pizzami/orders/services/order.py
@@ -1,11 +1,15 @@
 from rest_framework.utils.serializer_helpers import ReturnDict
 
+from pizzami.orders.selectors import get_or_create_cart
 from pizzami.orders.serializers import OrderInputSerializer, OrderDetailedOutputSerializer
 from pizzami.users.models import BaseUser
 
 
-def create_or_update_order(data: dict, user: BaseUser) -> ReturnDict:
-    serializer = OrderInputSerializer(data=data, context={"user": user.profile})
+def create_or_update_order(data: dict, user: BaseUser) -> ReturnDict | None:
+    cart = get_or_create_cart(user=user.profile)
+    if cart.items.count() == 0:
+        return None
+    serializer = OrderInputSerializer(data=data, context={"cart": cart})
     serializer.is_valid(raise_exception=True)
     serializer.save()
     response_serializer = OrderDetailedOutputSerializer(serializer.instance)
