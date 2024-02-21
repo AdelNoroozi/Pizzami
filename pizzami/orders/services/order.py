eae84b5a1eecbbd522209281f9afd48a92add5cb
adel13no@gmail.com <adel13no@gmail.com>
2024-02-21 13:04:38 +0330

    refactor(back): updating process handled through order service

--- pizzami/orders/services/order.py
+++ pizzami/orders/services/order.py
@@ -1,15 +1,20 @@
 from rest_framework.utils.serializer_helpers import ReturnDict
 
+from pizzami.orders.models import Order
 from pizzami.orders.selectors import get_or_create_cart
 from pizzami.orders.serializers import OrderInputSerializer, OrderDetailedOutputSerializer
 from pizzami.users.models import BaseUser
 
 
-def create_or_update_order(data: dict, user: BaseUser) -> ReturnDict | None:
+def create_or_update_order(data: dict, user: BaseUser) -> ReturnDict[Order] | None:
     cart = get_or_create_cart(user=user.profile)
     if cart.items.count() == 0:
         return None
-    serializer = OrderInputSerializer(data=data, context={"cart": cart})
+    orders = Order.objects.filter(cart=cart)
+    if orders.exists:
+        serializer = OrderInputSerializer(instance=orders.first(), data=data, partial=True, context={"cart": cart})
+    else:
+        serializer = OrderInputSerializer(data=data, context={"cart": cart})
     serializer.is_valid(raise_exception=True)
     serializer.save()
     response_serializer = OrderDetailedOutputSerializer(serializer.instance)
