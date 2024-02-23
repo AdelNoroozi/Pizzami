378b521e9c3ef8f66c78e854bfa74a7620ebffc2
adel13no@gmail.com <adel13no@gmail.com>
2024-02-23 11:26:44 +0330

    refactor(back): small modification in order list queryset

--- pizzami/orders/selectors/order.py
+++ pizzami/orders/selectors/order.py
@@ -6,6 +6,6 @@ from pizzami.users.models import Profile
 
 def get_orders(user_created: bool, user: Profile = None) -> QuerySet[Order]:
     if user_created:
-        return Order.objects.filter(cart__user=user)
+        return Order.objects.active().filter(cart__user=user)
     else:
         return Order.objects.all()
