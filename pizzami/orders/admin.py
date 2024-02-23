5d56dd738a9b7989de512d5db72cd98327b99030
adel13no@gmail.com <adel13no@gmail.com>
2024-02-23 12:00:26 +0330

    feat(back): cart registered in admin and url reference from order added

--- pizzami/orders/admin.py
+++ pizzami/orders/admin.py
@@ -3,9 +3,11 @@ from typing import Optional
 from admin_numeric_filter.admin import NumericFilterModelAdmin, SliderNumericFilter
 from django.contrib import admin
 from django.http import HttpRequest
+from django.urls import reverse
+from django.utils.html import format_html
 
 from pizzami.common.admin import BaseModelAdmin
-from pizzami.orders.models import Discount, Order, Payment
+from pizzami.orders.models import Discount, Order, Payment, CartItem, Cart
 
 
 @admin.register(Discount)
@@ -20,14 +22,42 @@ class DiscountAdmin(BaseModelAdmin, NumericFilterModelAdmin):
     ordering = BaseModelAdmin.ordering + ["absolute_value", "percentage_value"]
 
 
+class CartItemInlineAdmin(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+
+@admin.register(Cart)
+class CartAdmin(BaseModelAdmin):
+    inlines = (CartItemInlineAdmin,)
+    list_display = ["__str__", "is_alive", "total_value"] + BaseModelAdmin.list_display
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return False
+
+    def has_delete_permission(self, request: HttpRequest, obj: Optional = ...) -> bool:
+        return False
+
+    def has_change_permission(self, request: HttpRequest, obj: Optional = ...) -> bool:
+        return False
+
+
 @admin.register(Order)
 class OrderAdmin(BaseModelAdmin, NumericFilterModelAdmin):
-    list_display = ["__str__", "status", "final_value"] + BaseModelAdmin.list_display
+    list_display = ["__str__", "cart_link", "status", "final_value"] + BaseModelAdmin.list_display
     list_editable = BaseModelAdmin.list_editable + ["status"]
     list_filter = BaseModelAdmin.list_filter + ["status", "cart__user",
                                                 ("final_value", SliderNumericFilter)]
     search_fields = ["__str__"]
     ordering = BaseModelAdmin.ordering + ["final_value"]
+
+    def cart_link(self, obj):
+        cart = obj.cart
+        url = reverse("admin:orders_cart_change", args=[cart.id])
+        return format_html('<a href="{}">{}</a>', url, cart)
+
+    cart_link.short_description = "Cart"
+    cart_link.allow_tags = True
 
     def has_add_permission(self, request: HttpRequest) -> bool:
         return False
