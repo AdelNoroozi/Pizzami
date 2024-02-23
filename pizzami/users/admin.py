fd41071cc8860f49ef55d8fc816cab7001420099
adel13no@gmail.com <adel13no@gmail.com>
2024-02-23 18:46:06 +0330

    feat(back): address model admin & migrations added

--- pizzami/users/admin.py
+++ pizzami/users/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from pizzami.users.models import BaseUser, Profile
+from pizzami.common.admin import BaseModelAdmin
+from pizzami.users.models import BaseUser, Profile, Address
 
 admin.site.register(BaseUser)
 admin.site.register(Profile)
+
+
+@admin.register(Address)
+class AddressAdmin(BaseModelAdmin):
+    list_display = ["title", "user", "phone_number"] + BaseModelAdmin.list_display
+    list_filter = BaseModelAdmin.list_filter + ["user"]
+    search_fields = ["title", "address_str"]
