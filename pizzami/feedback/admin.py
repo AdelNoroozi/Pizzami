8a26a21ff6cb765665dc72195061c3ff5b59a583
adel13no@gmail.com <adel13no@gmail.com>
2024-02-24 19:16:21 +0330

    feat(back): comment registered in admin

--- pizzami/feedback/admin.py
+++ pizzami/feedback/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from pizzami.common.admin import BaseModelAdmin
+from pizzami.feedback.models import Comment
+
+
+@admin.register(Comment)
+class CommentModelAdmin(BaseModelAdmin):
+    list_display = ["__str__", "is_confirmed", "parent"] + BaseModelAdmin.list_display
+    list_editable = ["is_confirmed"] + BaseModelAdmin.list_editable
+    list_filter = BaseModelAdmin.list_filter + ["food", "user", "parent", "is_confirmed"]
+    search_fields = ["text"]
