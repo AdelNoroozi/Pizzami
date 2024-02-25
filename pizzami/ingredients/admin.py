701e731f2f36a1abe7ec4066c4aad7be3bba1c5d
adel13no@gmail.com <adel13no@gmail.com>
2024-02-25 19:13:20 +0330

    feat(back): is available field added to ingredient admin fields

--- pizzami/ingredients/admin.py
+++ pizzami/ingredients/admin.py
@@ -13,9 +13,10 @@ class IngredientCategoryAdmin(BaseModelAdmin):
 
 @admin.register(Ingredient)
 class IngredientAdmin(BaseModelAdmin, NumericFilterModelAdmin):
-    list_display = ["name", "category"] + BaseModelAdmin.list_display + ["remaining_units", "unit", "price"]
-    list_editable = BaseModelAdmin.list_editable + ["remaining_units"]
-    list_filter = BaseModelAdmin.list_filter + ["category", ("price", SliderNumericFilter),
+    list_display = ["name", "category", "is_available"] + BaseModelAdmin.list_display + ["remaining_units", "unit",
+                                                                                         "price"]
+    list_editable = BaseModelAdmin.list_editable + ["remaining_units", "is_available"]
+    list_filter = BaseModelAdmin.list_filter + ["category", "is_available", ("price", SliderNumericFilter),
                                                 ("remaining_units", SliderNumericFilter)]
     search_fields = ["name", "category__name"]
     ordering = BaseModelAdmin.ordering + ["remaining_units", "price"]
