7bd724797dde8c9f871fe1d523591762c9cbd23c
adel13no@gmail.com <adel13no@gmail.com>
2024-02-25 19:10:30 +0330

    feat(back): payment process now updates ingredients amount by cart

--- pizzami/orders/services/payment.py
+++ pizzami/orders/services/payment.py
@@ -2,6 +2,7 @@ from django.db import transaction
 from rest_framework.utils.serializer_helpers import ReturnDict
 
 from pizzami.foods.services import add_food_ordered_count
+from pizzami.ingredients.services import update_ingredients_amount_for_cart
 from pizzami.orders.models import Payment, Order, CartItem
 from pizzami.orders.serializers import PaymentGenericSerializer
 from pizzami.orders.services import change_order_status
@@ -17,6 +18,7 @@ def create_payment(data: dict) -> ReturnDict[Payment]:
     cart = order.cart
     cart.is_alive = False
     cart.save()
+    update_ingredients_amount_for_cart(cart=cart)
     items = CartItem.objects.filter(cart=cart)
     for item in items:
         add_food_ordered_count(food=item.food, count=item.count)
