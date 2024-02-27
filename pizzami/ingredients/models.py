62cc310b1ab3d5d4a1bcd3c263f41e40385c4f71
adel13no@gmail.com <adel13no@gmail.com>
2024-02-27 22:19:44 +0330

    refactor(back): ingredient check availability process moved to model save method

--- pizzami/ingredients/models.py
+++ pizzami/ingredients/models.py
@@ -39,11 +39,11 @@ class Ingredient(ImageIncludedBaseModel):
     def __str__(self):
         return self.name
 
-    def check_availability(self):
-        if self.stock_limit and self.stock_limit >= self.remaining_units:
-            self.is_available = False
-            self.save()
-        return self.is_available
+    def save(self, *args, **kwargs):
+        if self.auto_check_availability:
+            if self.stock_limit and self.stock_limit >= self.remaining_units:
+                self.is_available = False
+        super(Ingredient, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = _("Ingredient")
