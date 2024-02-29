3a5015d277881501563c0bcfa07149f51537d6e8
adel13no@gmail.com <adel13no@gmail.com>
2024-02-29 14:58:41 +0330

    feat(back): get comments service added

--- pizzami/feedback/services/__init__.py
+++ pizzami/feedback/services/__init__.py
@@ -1,2 +1,3 @@
 from pizzami.feedback.services.rating import create_or_update_rating
 from pizzami.feedback.services.comment import create_comment
+from pizzami.feedback.services.comment import get_comments
--- pizzami/feedback/services/comment.py
+++ pizzami/feedback/services/comment.py
@@ -1,7 +1,10 @@
-from rest_framework.utils.serializer_helpers import ReturnDict
+from django.http import QueryDict
+from rest_framework.utils.serializer_helpers import ReturnDict, ReturnList
 
 from pizzami.feedback.models import Comment
-from pizzami.feedback.serializers import CommentInputSerializer, CommentBaseOutputSerializer
+from pizzami.feedback.selectors import get_comments as get_comments_selector
+from pizzami.feedback.serializers import CommentInputSerializer, CommentBaseOutputSerializer, \
+    CommentDetailedOutputSerializer
 from pizzami.users.models import BaseUser
 
 
@@ -11,3 +14,14 @@ def create_comment(data: dict, user: BaseUser) -> ReturnDict[Comment]:
     serializer.save()
     response_serializer = CommentBaseOutputSerializer(serializer.instance, many=False)
     return response_serializer.data
+
+
+def get_comments(query_dict: QueryDict, is_user_staff: bool, user: BaseUser) -> ReturnList[Comment]:
+    if not is_user_staff:
+        queryset = get_comments_selector(user_created=True, user=user.profile)
+        serializer = CommentBaseOutputSerializer
+    else:
+        queryset = get_comments_selector(user_created=False)
+        serializer = CommentDetailedOutputSerializer
+    serializer(queryset=queryset, many=True)
+    return serializer.data
