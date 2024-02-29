37d936b798567f032131dbda4d23e9229353b0b0
adel13no@gmail.com <adel13no@gmail.com>
2024-02-29 13:01:58 +0330

    feat(back): create comment API added

--- pizzami/feedback/apis.py
+++ pizzami/feedback/apis.py
@@ -1,13 +1,14 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from pizzami.api.mixins import ApiAuthMixin, BasePermissionsMixin
 from pizzami.authentication.permissions import IsAuthenticatedAndNotAdmin
 from pizzami.feedback.documentation import RATE_FOOD_DESCRIPTION, RATE_FOOD_RESPONSES
-from pizzami.feedback.serializers import RatingInputSerializer
-from pizzami.feedback.services import create_or_update_rating
+from pizzami.feedback.serializers import RatingInputSerializer, CommentInputSerializer
+from pizzami.feedback.services import create_or_update_rating, create_comment
 
 
 class RateFoodAPI(ApiAuthMixin, BasePermissionsMixin, APIView):
@@ -26,3 +27,18 @@ class RateFoodAPI(ApiAuthMixin, BasePermissionsMixin, APIView):
         serializer.is_valid(raise_exception=True)
         create_or_update_rating(food_id=serializer.data["food"], user=request.user, rate=serializer.data["rate"])
         return Response(data={"message": "done"}, status=status.HTTP_200_OK)
+
+
+class CommentsAPI(ApiAuthMixin, BasePermissionsMixin, APIView):
+    permissions = {
+        "GET": [IsAuthenticated],
+        "POST": [IsAuthenticatedAndNotAdmin]
+    }
+
+    @extend_schema(
+        tags=["Feedback"],
+        request=CommentInputSerializer
+    )
+    def post(self, request):
+        comment_data = create_comment(data=request.data, user=request.user)
+        return Response(data=comment_data, status=status.HTTP_201_CREATED)
--- pizzami/feedback/urls.py
+++ pizzami/feedback/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from pizzami.feedback.apis import RateFoodAPI
+from pizzami.feedback.apis import RateFoodAPI, CommentsAPI
 
 urlpatterns = [
-    path('rate-food/', RateFoodAPI.as_view(), name="rate_food")
+    path('rate-food/', RateFoodAPI.as_view(), name="rate_food"),
+    path('comments/', CommentsAPI.as_view(), name="comments"),
 ]
