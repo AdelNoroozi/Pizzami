1a0b04af19d6759fb21dcf056c4df76aa05b8d8d
adel13no@gmail.com <adel13no@gmail.com>
2024-02-29 11:44:25 +0330

    feat(back): validations added to comment input serializer

--- pizzami/feedback/serializers/comment.py
+++ pizzami/feedback/serializers/comment.py
@@ -1,4 +1,6 @@
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from pizzami.feedback.models import Comment
 from pizzami.foods.serializers import FoodBaseOutputSerializer
@@ -8,6 +10,18 @@ class CommentInputSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ("food", "parent", "text")
+
+    def validate_parent(self, value):
+        if value.is_confirmed is not True:
+            raise ValidationError(_("a comment's parent must be confirmed."))
+
+        return value
+
+    def validate(self, data):
+        if data.get("parent") and data.get("parent").food != data.get("food"):
+            raise ValidationError(_("a comment's food must be the same as its parent."))
+
+        return data
 
     def save(self, **kwargs):
         self.validated_data["user"] = self.context.get("user")
