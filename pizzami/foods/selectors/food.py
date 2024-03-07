30470e1a8f76c2358a2d8426450efdbd9cea93ee
adel13no@gmail.com <adel13no@gmail.com>
2024-03-07 10:43:07 +0330

    refactor(back): normal search replaced with postgres search engine

--- pizzami/foods/selectors/food.py
+++ pizzami/foods/selectors/food.py
@@ -1,4 +1,5 @@
-from django.db.models import QuerySet, Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.db.models import QuerySet
 
 from pizzami.foods.models import Food
 from pizzami.users.models import Profile
@@ -15,7 +16,13 @@ def get_foods(return_all: bool, user_profile: Profile = None) -> QuerySet[Food]:
 
 
 def search_food(queryset: QuerySet[Food], search_param: str) -> QuerySet[Food]:
-    return queryset.filter(Q(name__icontains=search_param) | Q(description__icontains=search_param)).distinct()
+    search_vector = SearchVector("name", weight="A") + SearchVector("description", weight="C") + SearchVector(
+        "category__name", weight="B")
+    search_query = SearchQuery(search_param)
+    return queryset.annotate(
+        search=search_vector,
+        rank=SearchRank(search_vector, search_query)
+    ).filter(rank__gte=0.1).order_by("-rank")
 
 
 def order_foods(queryset: QuerySet[Food], order_param: str) -> QuerySet[Food]:
