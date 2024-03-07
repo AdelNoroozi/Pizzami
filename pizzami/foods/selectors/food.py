1392aaadf5feb161bd1dac6e8f1d770cccbaef5b
adel13no@gmail.com <adel13no@gmail.com>
2024-03-07 20:19:53 +0330

    feat(back): trigram similarity function added to search engine

--- pizzami/foods/selectors/food.py
+++ pizzami/foods/selectors/food.py
@@ -1,5 +1,5 @@
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
-from django.db.models import QuerySet
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
+from django.db.models import QuerySet, Q
 
 from pizzami.foods.models import Food
 from pizzami.users.models import Profile
@@ -16,13 +16,17 @@ def get_foods(return_all: bool, user_profile: Profile = None) -> QuerySet[Food]:
 
 
 def search_food(queryset: QuerySet[Food], search_param: str) -> QuerySet[Food]:
+    trigram_similarity = TrigramSimilarity("name", search_param) + \
+                         TrigramSimilarity("description", search_param) + \
+                         TrigramSimilarity("category__name", search_param)
     search_vector = SearchVector("name", weight="A") + SearchVector("description", weight="C") + SearchVector(
         "category__name", weight="B")
     search_query = SearchQuery(search_param)
     return queryset.annotate(
         search=search_vector,
-        rank=SearchRank(search_vector, search_query)
-    ).filter(rank__gte=0.1).order_by("-rank")
+        rank=SearchRank(search_vector, search_query),
+        trigram_similarity=trigram_similarity
+    ).filter(Q(trigram_similarity__gte=0.3) | Q(rank__gte=0.1)).distinct().order_by("-trigram_similarity", "-rank")
 
 
 def order_foods(queryset: QuerySet[Food], order_param: str) -> QuerySet[Food]:
