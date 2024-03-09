7b6c93c093f136408d26b37ac48dc63e2ff021b9
adel13no@gmail.com <adel13no@gmail.com>
2024-03-09 19:18:53 +0330

    refactor(back): custom pagination modified

--- pizzami/api/pagination.py
+++ pizzami/api/pagination.py
@@ -1,19 +1,7 @@
-from collections import OrderedDict
-
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.response import Response
 
 
 class FullPagination(PageNumberPagination):
+    page_size = 10
     page_size_query_param = 'page_size'
-
-    def get_paginated_response(self, data):
-        return Response(OrderedDict([
-            ('count', self.page.paginator.count),
-            ('next', self.get_next_link()),
-            ('previous', self.get_previous_link()),
-            ('total_pages', self.page.paginator.num_pages),
-            ('ok', data['ok']),
-            ('data', data['data']),
-            ('status', data['status']),
-        ]))
+    max_page_size = 100
