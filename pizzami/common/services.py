48fb646396da999e6c8defcf3dafc82263bd7e21
adel13no@gmail.com <adel13no@gmail.com>
2024-03-09 16:06:05 +0330

    refactor(back): change activation status common service modified

--- pizzami/common/services.py
+++ pizzami/common/services.py
@@ -1,11 +1,12 @@
 import uuid
 
+from django.db.models import QuerySet
 from django.db.models.base import ModelBase
 from rest_framework.generics import get_object_or_404
 
 
-def change_activation_status(obj_id: uuid, obj_cls: ModelBase) -> bool:
-    obj = get_object_or_404(obj_cls, id=obj_id)
+def change_activation_status(obj_id: uuid, queryset: QuerySet) -> bool:
+    obj = get_object_or_404(queryset, id=obj_id)
     is_active = obj.is_active
     obj.is_active = not is_active
     obj.save()
