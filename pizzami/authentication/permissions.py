bffb00d1346562a43a5242f06f710af435f7416e
adel13no@gmail.com <adel13no@gmail.com>
2024-03-09 12:00:37 +0330

    feat(back): is supeuser permission added

--- pizzami/authentication/permissions.py
+++ pizzami/authentication/permissions.py
@@ -4,3 +4,8 @@ from rest_framework.permissions import BasePermission
 class IsAuthenticatedAndNotAdmin(BasePermission):
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_authenticated and not request.user.is_staff)
+
+
+class IsSuperUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated and not request.user.is_superuser)
