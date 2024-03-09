c6f7f293ef2f4533642a754d0d89c19beaf5b8e3
adel13no@gmail.com <adel13no@gmail.com>
2024-03-09 15:29:08 +0330

    refactor(back): user admin modified

--- pizzami/users/admin.py
+++ pizzami/users/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from pizzami.common.admin import BaseModelAdmin
 from pizzami.users.models import BaseUser, Profile, Address
 
-admin.site.register(BaseUser)
 admin.site.register(Profile)
 
 
@@ -12,3 +11,11 @@ class AddressAdmin(BaseModelAdmin):
     list_display = ["title", "user", "phone_number"] + BaseModelAdmin.list_display
     list_filter = BaseModelAdmin.list_filter + ["user"]
     search_fields = ["title", "address_str"]
+
+
+@admin.register(BaseUser)
+class BaseUserAdmin(BaseModelAdmin):
+    list_display = ["email", "is_admin", "is_superuser"] + BaseModelAdmin.list_display
+    list_editable = BaseModelAdmin.list_editable + ["is_admin", "is_superuser"]
+    list_filter = BaseModelAdmin.list_filter + ["is_admin", "is_superuser"]
+    search_fields = ["email", "profile__public_name"]
