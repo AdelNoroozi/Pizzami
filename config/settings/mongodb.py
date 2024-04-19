bb952ed0bd5748d5c563781009b60cb3677c7bd2
adel13no@gmail.com <adel13no@gmail.com>
2024-04-19 21:18:12 +0430

    feat(back): profile document creates on first update in mongodb

--- config/settings/mongodb.py
+++ config/settings/mongodb.py
@@ -5,4 +5,4 @@ from config.env import env
 MONGODB_URL = env("MONGODB_URL", default="mongodb://root:password@localhost:27017")
 client = pymongo.MongoClient(MONGODB_URL)
 
-monogdb = client["pizzami_monogdb"]
+mongodb = client["pizzami_mongodb"]
--- pizzami/users/services/profile.py
+++ pizzami/users/services/profile.py
@@ -1,15 +1,28 @@
 from rest_framework.utils.serializer_helpers import ReturnDict
 
+from config.settings.mongodb import mongodb
 from pizzami.core.cache import redis_cache
 from pizzami.users.models import BaseUser, Profile
 from pizzami.users.selectors import get_profile as get_profile_selector
 from pizzami.users.serializers import ProfileOutputSerializer, ProfileUpdateSerializer
+
+profile_collection = mongodb["profiles"]
 
 
 @redis_cache(ttl=60 * 15)
 def get_profile(user: BaseUser) -> ReturnDict:
     profile = get_profile_selector(user=user)
     return ProfileOutputSerializer(profile).data
+
+
+def update_profile_custom_fields(profile_id: int, custom_fields: dict):
+    profile_document = profile_collection.find_one({"core_profile_id": profile_id})
+
+    if profile_document:
+        pass
+    else:
+        custom_fields["core_profile_id"] = profile_id
+        profile_collection.insert_one(custom_fields)
 
 
 def update_profile(user: BaseUser, data: dict) -> ReturnDict:
@@ -21,6 +34,7 @@ def update_profile(user: BaseUser, data: dict) -> ReturnDict:
     profile_qs = Profile.objects.filter(user=user)
     profile_qs.update(bio=data["bio"], public_name=data["public_name"])
     profile = profile_qs.first()
+
     if custom_fields is not None:
-        pass
+        update_profile_custom_fields(profile_id=profile.id, custom_fields=custom_fields)
     return ProfileOutputSerializer(profile).data
